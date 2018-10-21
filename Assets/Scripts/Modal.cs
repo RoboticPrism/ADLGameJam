9958a9bd14fee87180beaff03addc358473672f0
Ryan Stewart <stewart.rya@husky.neu.edu>
2018-10-21 14:40:16 -0400

    Added initial warning modal

---
+++ Assets/Scripts/Modal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Modal : MonoBehaviour {
+
+    [SerializeField]
+    Button button;
+
+	// Use this for initialization
+	void Start () {
+        button.onClick.AddListener(() => CloseModal());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CloseModal ()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
